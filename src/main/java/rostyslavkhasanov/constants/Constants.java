74af8d066f93ab57ef45ca77c9dd8f3e5e5c10a3
RostyslavKhasanov <rostyk.khasanov@gmail.com>
2019-12-05 23:29:32 +0200

    commit

--- src/main/java/rostyslavkhasanov/constants/Constants.java
+++ src/main/java/rostyslavkhasanov/constants/Constants.java
@@ -6,9 +6,6 @@ public class Constants {
     public static final String INTEGER_INPUT = "You have to input integer.";
     public static final String MENU_NUMBER_RANGE = "You have to input number between 1 and 5.";
 
-    //Error messages
-    public static final String HEAD_NOT_FOUND_BY_NAME = "Head of department not found";
-
     //menu list
     public static final String MENU =  "Menu \n" +
         "1 - Who is head of department \n" +
@@ -17,4 +14,9 @@ public class Constants {
         "4 - Show count of lectors \n" +
         "5 - Global search by template \n" +
         "Just pick number from the list: \n";
+
+    //Error messages
+    public static final String HEAD_NOT_FOUND_BY_NAME = "Head of department not found";
+
+
 }
